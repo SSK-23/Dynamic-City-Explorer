35e9950e260764e39e3a5eda685edb8ba5aef6a2
SSK-23 <sskhan20023@gmail.com>
2024-10-09 00:59:34 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    toggleButton.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
